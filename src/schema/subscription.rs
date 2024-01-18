13165a5228c33c267b9331ca5e497bb2b96e1ca1
Eugene Korir <korir16071@gmail.com>
2024-01-18 12:20:50 +0300

    Proper subscription example

--- src/schema/subscription.rs
+++ src/schema/subscription.rs
@@ -1,11 +1,18 @@
 use async_graphql::{Result, Subscription};
 use futures_util::Stream;
+use std::time::Duration;
 
 pub struct Subscription;
 
 #[Subscription]
 impl Subscription {
-    pub async fn interval(&self, n: i32) -> impl Stream<Item = i32> {
-        futures_util::stream::iter(0..n)
+    pub async fn interval(&self, n: i32) -> impl Stream<Item = String> {
+        async_stream::stream! {
+         let mut interval = tokio::time::interval(Duration::from_secs(2));
+         loop {
+             let n = interval.tick().await;
+             yield format!("{}", n.elapsed().as_secs());
+         }
+        }
     }
 }
